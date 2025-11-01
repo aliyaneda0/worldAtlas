2330c24e41a404b552ad12ee9baa453aa50e366f
aliya neda <137129762+aliyaneda0@users.noreply.github.com>
2025-11-01 23:04:27 +0530

    Add pages

---
+++ worldAtlas/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { Home } from './pages/Home'
+
+
+const router = createBrowserrouter([
+  {
+    path:"/",
+    element:<Home/>
+  },{
+    
+  }
+])
+
+function App() {
+ 
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
